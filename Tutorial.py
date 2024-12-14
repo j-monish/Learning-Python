37354dce9c1cc4402f86d8e60afff85a977fa620
Monish Jain <monishjain1008@gmail.com>
2024-12-14 19:26:37 +0530

    String Formatting

---
+++ Tutorial.py
@@ -0,0 +1,66 @@
+# # # # # # #Quote is used for strings. They are to be copied as they are.
+# # # # # # password =input("light:")
+# # # # # # if(light=="red"):
+# # # # # #     print("stop")
+# # # # # # elif(light=="yellow"):
+# # # # # #     print("wait")
+# # # # # # elif(light=="green"):
+# # # # # #     print("go")
+# # # # # # else:
+# # # # # #     print("light gone bad")
+
+# # # # # # dict1 = {"name": "monish", "phone": 9312}
+# # # # # # students = {"monish", "mohit"}
+# # # # # # # for key in students.
+# # # # # # #     print(key)
+# # # # # # print(students)
+# # # # # pi=22/7
+# # # # # print(pi)
+# # # # # print(type(pi))
+# # # # p=5
+# # # # r=6
+# # # # t=3
+# # # # a=p*r*t/100
+# # # # print(a)
+# # # # a=2e3
+# # # # print(a)
+# # # # print(type(a))
+# # # a = str(2.5555551e3)
+# # # print(a)       # 2000
+# # # print(type(a)) # <class 'int'>
+# # # a=2//3
+# # # print(a)
+# # # b=int(2/3)
+# # # print(b)
+# # a=2
+# # b=5
+# # m=f"a={a} \nb={b}"
+# # print(m)
+# # first="Mohan"
+# # last="Sharma"
+# # Name=f"{first} {last}"
+# # print(Name)
+# # print(Name[:])#end index is not included
+# # print(len(Name))
+# # my_food='samosa,jalebi'
+# # print(my_food)
+# # print("jalebi" not in my_food)
+# # print('salad' not in my_food)
+# # print(my_food.replace("samosa","salad"))
+# # print(dir(my_food))
+# # my_fav_sweet = "I love jalebi, kaju katli and laddu."
+# # print("jalebi" not in my_fav_sweet)
+# x=5
+# y=8
+# # print(f'I eat {x} fr and {y} veg in a')
+line='The Himalayas are one of the youngest mountain ranges on the planet'
+# print(line[4:13])
+# print(line[-30:-15])
+# string= "There are 9 planets in the solar system"; print(string.replace('9','8'))
+abc=f'{line[:13]} {line[-13:]}'
+print(abc)
+
+
+
+
+
