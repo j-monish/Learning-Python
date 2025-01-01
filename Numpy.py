f19bf5478775def0fb75f07083ed777d8b66696e
Monish Jain <monishjain1008@gmail.com>
2025-01-01 17:18:48 +0530

    Numpy

---
+++ Numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+# arr = np.array([1,2,3])
+# print(arr)
+# SIZE=100
+# n1=np.arange(SIZE)
+# n2=np.arange(SIZE)
+# n3=n1[:4]+n2[:4]
+# print(n3)
+# rev_q1 = np.array([10,12,9])
+# rev = np.array([[10,12,9],[15,11,13]])
+
+# re1=np.array([1,3,47,8])
+# re=np.arange(4,10,2)
+# print(np.square(re1))
+# print(re1+re)
+import pandas as pd
+df=pd.read_excel("AEC5_S1_marks.xlsx")
+print(df)
+
